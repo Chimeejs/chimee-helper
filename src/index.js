b9a649f42894c76bcca4f579ac077f09e4ee4dff
toxic-johann <353904974@qq.com>
2017-07-06 19:09:47 +0800

    [build] 0.1.5 what: why: how:

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ export * from 'toxic-utils';
 export * from 'toxic-predicate-functions';
 export * from 'chimee-helper-utils';
 export * from 'chimee-helper-events';
+export * from 'chimee-helper-dom';
